0979adf91ef0afd0489b8deb3342e88621d570ff
suvamligal <suvamligal@gmail.com>
2023-05-22 20:49:18 -0500

    initaial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int i =2;
+
+        if(i ==2){
+            System.out.println("2");
+        }
+        else
+            System.out.println("Other");
+    }
+}
